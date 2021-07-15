152902b21d9090227ade205696d6a916fd94c45d
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-15 19:15:12 +0300

    add requests

--- src/api/students.ts
+++ src/api/students.ts
@@ -1,7 +1,11 @@
 import { getStudentsRequest, getStudentsResponse } from '@models/students/getStudents'
+import { addStudentRequest, addStudentResponse } from '@models/students/addStudent'
+import { deleteStudentRequest, deleteStudentResponse } from '@models/students/deleteStudent'
 import { http } from '@services/httpClient'
 
 
-// export const getStudents = (data: getStudentsRequest) => http.get<getStudentsResponse>('/students/getStudents', data)
+export const getStudents = (data: getStudentsRequest) => http.get<getStudentsResponse>('/students/getStudents', data)
 
-
+export const addStudent = (data: addStudentRequest) => http.post<addStudentResponse>('/students/addStudent', data)
+// export const deleteStudent = (data: deleteStudentRequest) => 
+// http.delete<deleteStudentResponse>('/students/deleteStudent', data)
