a45979970c86819ec399b452f8b6df72c094f0f0
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-16 19:51:34 +0300

    fix onCLick in button

--- src/pages/Students/components/StudentsTitle/StudentsTitle.tsx
+++ src/pages/Students/components/StudentsTitle/StudentsTitle.tsx
@@ -2,7 +2,6 @@ import React, {FC} from 'react'
 import { PATH_NEWSTUDENT } from '@consts/routes'
 
 import Button from '@components/ui/Button'
-import browserHistory from '@utils/browserHistory'
 import { ReactComponent as PlusIcon } from '@assets/icons/plus.svg'
 
 import * as SC from './styled'
@@ -12,7 +11,8 @@ const StudentsTitle: FC = () => {
     return <SC.StudentsTitleContainer>
         <SC.StudentsTitle>Студенты</SC.StudentsTitle>
         <SC.ButtonWrapper to={PATH_NEWSTUDENT}>
-            <Button text="Добавить студента" onClick={browserHistory.goBack} icon={PlusIcon}></Button>
+            <Button text="Добавить студента" onClick={() => console.log('hi')
+            } icon={PlusIcon}></Button>
         </SC.ButtonWrapper> 
     </SC.StudentsTitleContainer>
 }
