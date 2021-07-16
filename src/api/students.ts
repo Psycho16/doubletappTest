b453a84e3adcb7b8098b1e1b55e791fbd5ad99a1
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-16 19:28:31 +0300

    fix types of requests

--- src/api/students.ts
+++ src/api/students.ts
@@ -1,11 +1,11 @@
-import { getStudentsRequest, getStudentsResponse } from '@models/students/getStudents'
-import { addStudentRequest, addStudentResponse } from '@models/students/addStudent'
-import { deleteStudentRequest, deleteStudentResponse } from '@models/students/deleteStudent'
+import { getStudentsRequest, getStudentsResponse } from '@models/students/ApiModels/getStudents'
+import { addStudentRequest, addStudentResponse } from '@models/students/ApiModels/addStudent'
+import { deleteStudentRequest, deleteStudentResponse } from '@models/students/ApiModels/deleteStudent'
 import { http } from '@services/httpClient'
 
 
 export const getStudents = (data: getStudentsRequest) => http.get<getStudentsResponse>('/students/getStudents', data)
 
 export const addStudent = (data: addStudentRequest) => http.post<addStudentResponse>('/students/addStudent', data)
-// export const deleteStudent = (data: deleteStudentRequest) => 
-// http.delete<deleteStudentResponse>('/students/deleteStudent', data)
+export const deleteStudent = (data: deleteStudentRequest) => 
+http.delete<deleteStudentResponse>('/students/deleteStudent', data)
