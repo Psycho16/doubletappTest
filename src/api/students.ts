e6b33138df98efd3bd344447431a0a72318e03eb
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-21 15:41:30 +0300

    add api functions for requests

--- src/api/students.ts
+++ src/api/students.ts
@@ -1,11 +1,10 @@
-import { getStudentsRequest, getStudentsResponse } from '@models/students/ApiModels/getStudents'
-import { addStudentRequest, addStudentResponse } from '@models/students/ApiModels/addStudent'
-import { deleteStudentRequest, deleteStudentResponse } from '@models/students/ApiModels/deleteStudent'
+import { getStudentsResponse } from '@models/students/ApiModels/getStudents'
+import { addStudentResponse } from '@models/students/ApiModels/addStudent'
+import { deleteStudentResponse } from '@models/students/ApiModels/deleteStudent'
 import { http } from '@services/httpClient'
 
 
-export const getStudents = (data: getStudentsRequest) => http.get<getStudentsResponse>('/students/getStudents', data)
+export const getStudents = () => http.get<getStudentsResponse>('/students')
 
-export const addStudent = (data: addStudentRequest) => http.post<addStudentResponse>('/students/addStudent', data)
-export const deleteStudent = (data: deleteStudentRequest) => 
-http.delete<deleteStudentResponse>('/students/deleteStudent', data)
+export const addStudent = (data: FormData) => http.post<addStudentResponse>('/students', data)
+export const deleteStudent = (id: number) => http.delete<deleteStudentResponse>(`/students/${id}`)
