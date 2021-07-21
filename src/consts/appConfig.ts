bc6edc7e4848a757725de2dfaa2088a8c39766fe
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-21 15:35:23 +0300

    fix token and secret

--- src/consts/appConfig.ts
+++ src/consts/appConfig.ts
@@ -1,7 +1,8 @@
 export default {
   api: {
     baseUrl: process.env.REACT_APP_API_BASE_URL,
-    secret: process.env.REACT_APP_API_SECRET
+    secret: process.env.REACT_APP_API_SECRET,
+    token: process.env.REACT_APP_TOKEN
   },
   localStorage: {
     authTokenStorageName: 'authToken',
