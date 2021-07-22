b49033ca69e30eb7007f5550ea2bd0de6a54d02f
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-22 16:08:31 +0300

    fix naming

--- src/api/students.ts
+++ src/api/students.ts
@@ -1,10 +1,10 @@
-import { getStudentsResponse } from '@models/students/ApiModels/getStudents'
-import { addStudentResponse } from '@models/students/ApiModels/addStudent'
-import { deleteStudentResponse } from '@models/students/ApiModels/deleteStudent'
+import { GetStudentsResponse } from '@models/students/ApiModels/getStudents'
+import { AddStudentResponse } from '@models/students/ApiModels/addStudent'
+import { DeleteStudentResponse } from '@models/students/ApiModels/deleteStudent'
 import { http } from '@services/httpClient'
 
 
-export const getStudents = () => http.get<getStudentsResponse>('/students')
+export const getStudents = () => http.get<GetStudentsResponse>('/students')
 
-export const addStudent = (data: FormData) => http.post<addStudentResponse>('/students', data)
-export const deleteStudent = (id: number) => http.delete<deleteStudentResponse>(`/students/${id}`)
+export const addStudent = (data: FormData) => http.post<AddStudentResponse>('/students', data)
+export const deleteStudent = (id: number) => http.delete<DeleteStudentResponse>(`/students/${id}`)
