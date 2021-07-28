da695a277eede215848d72c9f2f73621264d44a5
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-28 13:35:15 +0300

    delete Login page

--- src/pages/Login/components/RegisterForm/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/macro'
-
-
-export const Base = styled.div`
-  margin-top: 40px;
-`
--- src/pages/Login/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/macro'
-
-
-export const Base = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
