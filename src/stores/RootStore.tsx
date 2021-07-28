6b6aed46411429ef9fc054b8f6c327e281a4848c
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-28 13:39:45 +0300

    delete UserStore and AuthStore

--- src/stores/RootStore.tsx
+++ src/stores/RootStore.tsx
@@ -1,16 +1,10 @@
-import AuthStore from './Auth/AuthStore'
-import UserStore from './common/UserStore'
 import StudentsStore from './Students/StudentsStore'
 
 
 export default class RootStore {
-  userStore: UserStore
-  authStore: AuthStore
   studentsStore: StudentsStore
 
   constructor() {
-    this.userStore = new UserStore(this)
-    this.authStore = new AuthStore(this)
     this.studentsStore = new StudentsStore(this)
   }
 }
