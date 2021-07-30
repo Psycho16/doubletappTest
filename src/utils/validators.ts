237b2390034c56898b644dd5f4d03b08085778e4
PsychoXgod <lobanov2000k@yandex.ru>
2021-07-30 14:02:20 +0300

    fix validators and made new validator for formFields

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,37 +1,45 @@
+import { IFormInput } from "@models/students/EntityModels/student"
+
 /* eslint-disable max-len */
 /* eslint-disable no-useless-escape */
-export const isEmailValidRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-export function isEmailValid(email: string): boolean {
+export const isEmailValid = (email: string): boolean => {
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return regex.test(email)
 }
 
-// export const isDateValidRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
-export const isDateValidRegex = /[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])/
-
-export function isDateValid(date: string): boolean {
-  const regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+export const isDateValid = (date: string): boolean => {
+  const regex =  /[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])/
   return regex.test(date)
 }
 
-export function isPhoneValid(phone: string): boolean {
-  const regex = /^[+]\d{1,2}\s[(]{0,1}[0-9]{1,4}[)]{0,1}[ \s\./0-9]*$/g
-  return regex.test(phone)
-}
-
-export function isValidURL(string: string): boolean {
-  const regex = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?' + // fragment locator
-    '(\\@[-a-z\\d_]*)?$','i') // tiktok user link
-  return regex.test(string)
-}
-
 export function isNumberValid(number: string): boolean {
-  const regex = /^[\d]*$/
+  const regex = /^-?[\d]*$/
   return regex.test(number)
+}
+
+export function isEmptyField(field: string): boolean {
+  return field === ""
+}
+
+export function isValidFields(data: IFormInput, isReadyToSubmit: boolean) {
+    
+    if ((isEmptyField(data.specialty))
+        || (isEmptyField(data.group))
+        || (isEmptyField(data.color))
+        || (isEmptyField(data.color))
+        || (isEmptyField(data.email))
+        || (isEmptyField(data.name))
+        || (isEmptyField(data.rating))
+        || (isEmptyField(data.birthday)) 
+        || (+data.rating < 1)
+        || (+data.rating > 999)
+        || (!isNumberValid(data.rating)) 
+        || (!isDateValid(data.birthday))
+        || (!isEmailValid(data.email))
+        ) 
+          {
+            isReadyToSubmit = false
+          }
+
+  return isReadyToSubmit
 }
