25f382929eb72fb5f6332e90314165b8b42017f4
PsychoXgod <lobanov2000k@yandex.ru>
2021-08-04 11:55:23 +0300

    add new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
-import { PATH_NEW_STUDENT, PATH_ROOT } from "@consts/routes"
+import { PATH_NEW_STUDENT, PATH_STUDENTS, PATH_STUDENT_ID } from "@consts/routes"
 import ScrollToTop from "ScrollToTop"
 import StudentsPage from "@pages/Students"
 import NotFoundPage from "@pages/NotFound"
 import NewStudentPage from "@pages/NewStudent"
+import StudentPage from "@pages/StudentPage/StudentPage"
 
 
 const Router = (): JSX.Element => {
@@ -13,18 +14,23 @@ const Router = (): JSX.Element => {
       <ScrollToTop />
       
       <Switch>
-        <Route exact path={PATH_ROOT}>
+        <Route exact path="/">
+          <Redirect to={PATH_STUDENTS} />
+        </Route>
+        <Route exact path={PATH_STUDENTS}>
           <StudentsPage />
         </Route>
         <Route path={PATH_NEW_STUDENT}>
           <NewStudentPage />
         </Route>
 
+        <Route exact path={PATH_STUDENT_ID}>
+          <StudentPage/>
+        </Route>
+
         <Route>
           <NotFoundPage />
         </Route>
-
-        <Redirect to={PATH_ROOT} />
       </Switch>
     </BrowserRouter>
   )
--- src/consts/routes.tsx
+++ src/consts/routes.tsx
@@ -1,3 +1,4 @@
 export const PATH_ROOT = '/'
 export const PATH_STUDENTS = '/students'
 export const PATH_NEW_STUDENT = '/new-student'
+export const PATH_STUDENT_ID = `${PATH_STUDENTS}/:id`
